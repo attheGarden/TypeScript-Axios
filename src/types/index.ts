a4a2983ccd78010c090a6b3a9b2f6e370583739e
wuchao <2865178654@qq.com>
2022-07-11 19:52:53 +0800

    1.responseHeader处理 2.responseData处理 3.error处理

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,9 +7,30 @@ export type Method = 'get' | 'GET'
   | 'patch' | 'PATCH'
 
 export interface AxiosRequestConfig {
-  url: string,
-  method?: Method,
-  data?: any,
-  params?: any,
+  url: string
+  method?: Method
+  data?: any
+  params?: any
   headers?: any
+  responseType?: XMLHttpRequestResponseType
+  timeout?: number
+}
+
+export interface AxiosResponse {
+  data: any
+  status: number
+  statusText: string
+  headers: any
+  config: AxiosRequestConfig
+  request: any
+}
+
+export interface AxiosPromise extends Promise<AxiosResponse> {}
+
+export interface AxiosError extends Error {
+  isAxiosError: boolean
+  config: AxiosRequestConfig
+  code?: string | null
+  request?: any
+  response?: AxiosResponse
 }
